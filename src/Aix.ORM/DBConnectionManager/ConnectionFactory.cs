d9e6df7940ce58239e87b3c35fc1d204c7530a23
linzhiqiang <360779475@qq.com>
2019-11-17 21:16:46 +0800

    在基类确定数据库连接 通过反射 MySql.Data.MySqlClient.MySqlConnection System.Data.SqlClient.SqlConnection

--- src/Aix.ORM/DBConnectionManager/ConnectionFactory.cs
+++ src/Aix.ORM/DBConnectionManager/ConnectionFactory.cs
@@ -1,4 +1,6 @@
-﻿using System.Data;
+﻿using Aix.ORM.Common;
+using System;
+using System.Data;
 
 namespace Aix.ORM.DBConnectionManager
 {
@@ -22,13 +24,42 @@ namespace Aix.ORM.DBConnectionManager
         IDbConnection CreateConnection(string connectionString);
     }
 
-    //public class MsSqlConnectionFactory : IConnectionFactory
-    //{
-    //    public IDbConnection CreateConnection(string connectionString)
-    //    {
-    //        return new SqlConnection(connectionString);
-    //    }
-    //}
+    public abstract class BaseConnectionFactory
+    {
+        IDbConnection CreateConnection(string connectionString , ORMDBType dbType )
+        {
+            IDbConnection connection = null;
+            switch (dbType)
+            {
+                case ORMDBType.MsSql:
+                    connection = CreateMsSqlSqlConnection(connectionString);
+                    break;
+                case ORMDBType.MySql:
+                    connection = CreateMySqlConnection(connectionString);
+                    break;
+                case ORMDBType.Oracle:
+                    connection = CreateOracleSqlSqlConnection(connectionString);
+                    break;
+                default:
+                    break;
+            }
 
+            return connection;
+        }
 
+        public virtual IDbConnection CreateMsSqlSqlConnection(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual IDbConnection CreateMySqlConnection(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual IDbConnection CreateOracleSqlSqlConnection(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
