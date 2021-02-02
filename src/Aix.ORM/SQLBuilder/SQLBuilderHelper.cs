50b49e10802ee939c9909e407b56358a3b23f759
linzhiqiang <linzhiqiang@shandagames.com>
2021-02-02 12:18:51 +0800

    优化

--- src/Aix.ORM/SQLBuilder/SQLBuilderHelper.cs
+++ src/Aix.ORM/SQLBuilder/SQLBuilderHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Aix.ORM.Common;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,12 @@ namespace Aix.ORM.SQLBuilder
 {
     public static class SQLBuilderHelper
     {
-        private static Dictionary<Type, string> _InsertSqlCache = new Dictionary<Type, string>();
-        private static Dictionary<Type, string> _UpdateSqlCache = new Dictionary<Type, string>();
-        private static Dictionary<Type, string> _GetByPkSqlCache = new Dictionary<Type, string>();
-        private static Dictionary<Type, string> _DeleteByPkSqlCache = new Dictionary<Type, string>();
-        private static Dictionary<Type, string> _ReplaceSqlCache = new Dictionary<Type, string>();
-        private static Dictionary<Type, string> _AllColumnsSqlCache = new Dictionary<Type, string>();
+        private static ConcurrentDictionary<Type, string> _InsertSqlCache = new ConcurrentDictionary<Type, string>();
+        private static ConcurrentDictionary<Type, string> _UpdateSqlCache = new ConcurrentDictionary<Type, string>();
+        private static ConcurrentDictionary<Type, string> _GetByPkSqlCache = new ConcurrentDictionary<Type, string>();
+        private static ConcurrentDictionary<Type, string> _DeleteByPkSqlCache = new ConcurrentDictionary<Type, string>();
+        private static ConcurrentDictionary<Type, string> _ReplaceSqlCache = new ConcurrentDictionary<Type, string>();
+        private static ConcurrentDictionary<string, string> _AllColumnsSqlCache = new ConcurrentDictionary<string, string>();
 
 
         public static string GetInsertSql(BaseEntity model, ORMDBType dbType)
@@ -39,14 +40,8 @@ namespace Aix.ORM.SQLBuilder
             Type type = model.GetType();
             if (!_GetByPkSqlCache.ContainsKey(type))
             {
-                lock (_GetByPkSqlCache)
-                {
-                    if (!_GetByPkSqlCache.ContainsKey(type))
-                    {
-                        EntityMeta metadeta = EntityReflect.GetDefineInfoFromType(type);
-                        _GetByPkSqlCache.Add(type, SQLBuilderFactory.Instance.GetSQLBuilder(dbType).BuildSelectByPkSql(metadeta));
-                    }
-                }
+                EntityMeta metadeta = EntityReflect.GetDefineInfoFromType(type);
+                _GetByPkSqlCache.TryAdd(type, SQLBuilderFactory.Instance.GetSQLBuilder(dbType).BuildSelectByPkSql(metadeta));
             }
             return _GetByPkSqlCache[type];
         }
@@ -56,14 +51,8 @@ namespace Aix.ORM.SQLBuilder
             Type type = model.GetType();
             if (!_DeleteByPkSqlCache.ContainsKey(type))
             {
-                lock (_DeleteByPkSqlCache)
-                {
-                    if (!_DeleteByPkSqlCache.ContainsKey(type))
-                    {
-                        EntityMeta metadeta = EntityReflect.GetDefineInfoFromType(type);
-                        _DeleteByPkSqlCache.Add(type, SQLBuilderFactory.Instance.GetSQLBuilder(dbType).BuildDeleteByPkSql(metadeta));
-                    }
-                }
+                EntityMeta metadeta = EntityReflect.GetDefineInfoFromType(type);
+                _DeleteByPkSqlCache.TryAdd(type, SQLBuilderFactory.Instance.GetSQLBuilder(dbType).BuildDeleteByPkSql(metadeta));
             }
             return _DeleteByPkSqlCache[type];
         }
@@ -81,12 +70,9 @@ namespace Aix.ORM.SQLBuilder
             {
                 EntityMeta metadeta = EntityReflect.GetDefineInfoFromType(t);
                 string sql = SQLBuilderFactory.Instance.GetSQLBuilder(dbType).BuildReplaceInsertSQL(metadeta);
-                lock (_ReplaceSqlCache)
+                if (!_ReplaceSqlCache.ContainsKey(t))
                 {
-                    if (!_ReplaceSqlCache.ContainsKey(t))
-                    {
-                        _ReplaceSqlCache.Add(t, sql);
-                    }
+                    _ReplaceSqlCache.TryAdd(t, sql);
                 }
             }
             return _ReplaceSqlCache[t];
@@ -102,18 +88,12 @@ namespace Aix.ORM.SQLBuilder
         public static string GetAllColumns(Type entityType, ORMDBType dbType, string prefix)
         {
 
-            Type t = entityType;
+            string t = entityType.FullName + prefix;
             if (!_AllColumnsSqlCache.ContainsKey(t))
             {
-                EntityMeta metadata = EntityReflect.GetDefineInfoFromType(t);
+                EntityMeta metadata = EntityReflect.GetDefineInfoFromType(entityType);
                 string sql = SQLBuilderFactory.Instance.GetSQLBuilder(dbType).GetAllColumns(metadata, prefix);
-                lock (_AllColumnsSqlCache)
-                {
-                    if (!_AllColumnsSqlCache.ContainsKey(t))
-                    {
-                        _AllColumnsSqlCache.Add(t, sql);
-                    }
-                }
+                _AllColumnsSqlCache.TryAdd(t, sql);
             }
             return _AllColumnsSqlCache[t];
         }
@@ -132,13 +112,7 @@ namespace Aix.ORM.SQLBuilder
             {
                 EntityMeta metadeta = EntityReflect.GetDefineInfoFromType(type);
                 string sql = SQLBuilderFactory.Instance.GetSQLBuilder(dbType).BuildInsertSql(metadeta);
-                lock (_InsertSqlCache)
-                {
-                    if (!_InsertSqlCache.ContainsKey(type))
-                    {
-                        _InsertSqlCache.Add(type, sql);
-                    }
-                }
+                _InsertSqlCache.TryAdd(type, sql);
             }
             return _InsertSqlCache[type];
         }
@@ -157,13 +131,7 @@ namespace Aix.ORM.SQLBuilder
             {
                 EntityMeta metadeta = EntityReflect.GetDefineInfoFromType(type);
                 string sql = SQLBuilderFactory.Instance.GetSQLBuilder(dbType).BuildUpdateSql(metadeta);
-                lock (_UpdateSqlCache)
-                {
-                    if (!_UpdateSqlCache.ContainsKey(type))
-                    {
-                        _UpdateSqlCache.Add(type, sql);
-                    }
-                }
+                _UpdateSqlCache.TryAdd(type, sql);
             }
             return _UpdateSqlCache[type];
         }
